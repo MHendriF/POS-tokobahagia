81953244cf5d142382e25c6e164e711476ded976
M Hendri Febriansyah <muhamad.hendri.febriansyah14@mhs.if.its.ac.id>
2017-09-21 13:58:59 +0700

    add favicon

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -8,6 +8,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" href="{{ asset("img/favicon2.ico") }}" type="image/x-icon">
+
         <title>@yield('title')</title>
 
         <!-- Bootstrap -->
